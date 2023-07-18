9c1f76b1b3fa30ee6e3bee74f397ce02b7bb5b96
ddathvhe151280 <87832538+hoangvandat2001@users.noreply.github.com>
2023-07-18 17:34:24 +0700

    ﻿lastest
    lastest

--- Assets/Scripts/NewGameButton.cs
+++ Assets/Scripts/NewGameButton.cs
@@ -9,20 +9,29 @@ using UnityEngine.UI;
 public class NewGameButton : MonoBehaviour
 {
     public Button button;
+    public Text highScoreText;
+    public Text pointText;
     // Start is called before the first frame update
     void Start()
     {
         button.onClick.AddListener(ClickAction);
+
+        int currentScore = GameController.Instance.GetScore();
+        pointText.text = "Score: " + currentScore.ToString();
+
+        int highScore = GameController.Instance.GetHighScore();
+        highScoreText.text = "High Score: " + highScore.ToString();
+
     }
 
     private void ClickAction()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
