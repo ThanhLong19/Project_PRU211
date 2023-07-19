5af51d330b304bd52413aeb373345d2f127d8aad
KWANG204\devkw <kieuquang.official@gmail.com>
2023-07-19 10:28:01 +0700

    Quangtk4 commit src4

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,24 +1,23 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NewGameButton : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
-    public Button button;
+    public Button menuButton;
     // Start is called before the first frame update
     void Start()
     {
-        button.onClick.AddListener(ClickAction);
+        menuButton.onClick.AddListener(ClickAction);
     }
 
     private void ClickAction()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
+
 
     // Update is called once per frame
     void Update()
