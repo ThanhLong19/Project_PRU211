0e70fb341df88654d650c8c21107d982f7e4712f
KWANG204\devkw <kieuquang.official@gmail.com>
2023-07-20 16:06:23 +0700

    Quangtk4 commit src6

--- Assets/Scripts/NewGameButton.cs
+++ Assets/Scripts/NewGameButton.cs
@@ -8,22 +8,34 @@ using UnityEngine.UI;
 
 public class NewGameButton : MonoBehaviour
 {
+    public Button quit;
+    public Button ButtomMenu;
     public Button button;
     public Text highScoreText;
     public Text pointText;
     // Start is called before the first frame update
     void Start()
     {
-        button.onClick.AddListener(ClickAction);
+        button.onClick.AddListener(ClickNewGameAction);
+        ButtomMenu.onClick.AddListener(ClickMenuAction);
+        quit.onClick.AddListener(ClickQuitAction);
         int currentScore = GameController.Instance.GetScore();
         pointText.text = "Score: " + currentScore.ToString();
         int highScore = GameController.Instance.GetHighScore();
         highScoreText.text = "High Score: " + highScore.ToString();
     }
 
-    private void ClickAction()
+    private void ClickNewGameAction()
     {
         SceneManager.LoadScene(1);
+    }
+    private void ClickMenuAction()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void ClickQuitAction()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
